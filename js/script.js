dd5a0f26a69c21c24e0058e3ce11475d8b61f791
wa0000 <cf8e1p@gmail.com>
2021-07-21 11:56:28 +0900

    最初

---
+++ js/script.js
@@ -0,0 +1,11 @@
+var $slide = $(".slide")
+  .slick({
+    fade: true,
+    speed: 1500,
+    autoplaySpeed: 3000,
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    infinite: true
+  });
