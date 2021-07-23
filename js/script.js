f00e848ffb16e98700df1f944694bcccf0ab5204
wa0000 <cf8e1p@gmail.com>
2021-07-24 05:29:30 +0900

    一番下の三角形が動かねえ

--- js/script.js
+++ js/script.js
@@ -1,8 +1,8 @@
 var $slide = $(".slide")
   .slick({
     fade: true,
-    speed: 1500,
-    autoplaySpeed: 3000,
+    speed: 2500,
+    autoplaySpeed: 2500,
     arrows: false,
     autoplay: true,
     slidesToShow: 1,
